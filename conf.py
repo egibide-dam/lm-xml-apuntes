f70310114ff2ae9edbf0fc17de6e478b0836a532
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2018-11-05 09:48:31 +0100

    Añadido el botón de copiar

--- conf.py
+++ conf.py
@@ -39,7 +39,7 @@ release = '2018'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_markdown_tables',
+    'sphinx_copybutton'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
