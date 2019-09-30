b6defdf0209e8c9651a1d572bfdfc38309248b6d
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2019-09-30 09:29:09 +0200

    Botón de copiar más grande

--- conf.py
+++ conf.py
@@ -90,6 +90,10 @@ html_title = "Lenguajes de marcas"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = [
+    'css/copybutton.css',
+]
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
